e229a394d22095c53e6275bad2ba09c7994fb5bf
wang-ps <wangps@hotmail.com>
2016-04-26 23:34:41 +0800

    change bilateral normal filter example to batch mode

--- Example/BilateralNormalFilter/BilateralNormalFilter/main.cpp
+++ Example/BilateralNormalFilter/BilateralNormalFilter/main.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3) return 0;
-	
-	string filename(argv[1]), filename1(argv[2]);
-	Eigen::MatrixXd V;
-	Eigen::MatrixXi F;
-	LibGP::read_mesh(filename, V, F);
+	if (argc < 2) return 0;
+	for (int i = 1; i < argc; i++)
+	{
+		string filename(argv[i]);
 
-	Eigen::MatrixXd V1;
-	LibGP::bilateral_normal_filter(V1, V, F);
+		Eigen::MatrixXd V; Eigen::MatrixXi F;
+		LibGP::read_mesh(filename, V, F);
 
-	LibGP::write_mesh(filename1, V1, F);
+		Eigen::MatrixXd V1;
+		LibGP::bilateral_normal_filter(V1, V, F, 0.35, 1, 3);
+
+		filename.insert(filename.rfind('.'), "_bilateral");
+		LibGP::write_mesh(filename, V1, F);
+	}
 
 	return 0;
 }
