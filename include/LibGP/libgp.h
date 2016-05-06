a762d067f8131a4ec963df5729172714b77ed50f
wang-ps <wangps@hotmail.com>
2016-05-07 00:17:56 +0800

    libgp include file

---
+++ include/LibGP/libgp.h
@@ -0,0 +1,37 @@
+// global
+#include "libgp_global.h"
+
+// read and write
+#include "read_mesh.h"
+#include "write_mesh.h"
+#include "write_png.h"
+
+// connectivity
+#include "list_to_adj.h"
+#include "adj_to_list.h"
+#include "compute_adj_matrix.h"
+#include "compute_face_face_ringv.h"
+#include "compute_vtx_face_ring.h"
+#include "compute_vtx_ring.h"
+
+// basic computation
+#include "compute_avg_edge_length.h"
+#include "compute_face_normal.h"
+#include "compute_face_center.h"
+#include "compute_normal.h"
+
+// eigen wrapper
+#include "slice.h"
+#include "speye.h"
+#include "eigen_solver.h"
+#include "normalize_colwise.h"
+
+// tools
+#include "color_map.h"
+#include "get_filename.h"
+#include "get_all_filenames.h"
+#include "console.h"
+
+// mesh filtering
+#include "bilateral_normal_filter.h"
+#include "reconstruct_mesh_LS.h"
