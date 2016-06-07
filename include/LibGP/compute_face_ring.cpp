ddb6fd960342ec8d7ee7df48e13d25198fd324e1
wang-ps <wangps@hotmail.com>
2016-06-07 16:13:43 +0800

    update compute_face_ring

--- include/LibGP/compute_face_ring.cpp
+++ include/LibGP/compute_face_ring.cpp
@@ -1,4 +1,5 @@
 #include "compute_face_ring.h"
+#include "compute_edge_face_ring.h"
 #include "compute_edge_face_adj.h"
 #include "adj_to_list.h"
 
@@ -7,10 +8,26 @@ namespace LibGP
 	LIBGP_INLINE void compute_face_ring(
 		vecveci& vec, const MatrixXi& F)
 	{
-		SMatrixf A;
-		compute_edge_face_adj(A, F);
+// 		SMatrixf A;
+// 		compute_edge_face_adj(A, F);
+// 
+// 		SMatrixf B = A.transpose() * A;
+// 		adj_to_list(vec, B);
 
-		SMatrixf B = A.transpose() * A;
-		adj_to_list(vec, B);
+		std::unordered_map<int, Vector2i> E2F;
+		compute_edge_face_ring(E2F, F);
+
+		vec.resize(F.cols());
+		for (auto& ef : E2F)
+		{
+			int f0 = ef.second[0];
+			int f1 = ef.second[1];
+			
+			// boundary edge
+			if (f0 == -1 || f1 == -1) continue;
+
+			vec[f0].push_back(f1);
+			vec[f1].push_back(f0);
+		}
 	}
 }
