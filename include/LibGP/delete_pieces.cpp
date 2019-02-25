0f0435d2b105fcfcb5c71a076cd507ce87f3c88a
wang-ps <wangps@hotmail.com>
2019-02-25 13:46:46 +0800

    refine delete_pieces

--- include/LibGP/delete_pieces.cpp
+++ include/LibGP/delete_pieces.cpp
@@ -54,14 +54,46 @@ void delete_pieces(MatrixXf& V, MatrixXi& F, int min_num) {
 
   /* mark invalid faces */
   vector<int> invalid_piece_id;
-  if (min_num < 0) {
-    min_num = *std::max_element(piece_num.begin(), piece_num.end());
-  }
-  for (int i = 0; i < piece_num.size(); i++) {
-    if (piece_num[i] < min_num) {
-      invalid_piece_id.push_back(i);
+  //// version 1: keep the component with the largest face number
+  //if (min_num < 0) {
+  //  min_num = *std::max_element(piece_num.begin(), piece_num.end());
+  //}
+  //for (int i = 0; i < piece_num.size(); i++) {
+  //  if (piece_num[i] < min_num) {
+  //    invalid_piece_id.push_back(i);
+  //  }
+  //}
+  //// end of version 1
+
+  // version 2: keep the largest component
+  int num = piece_num.size();
+  MatrixXf bbox_min = MatrixXf::Constant(3, num, std::numeric_limits<float>::max());
+  MatrixXf bbox_max = MatrixXf::Constant(3, num, -std::numeric_limits<float>::max());
+  for (int i = 0; i < nf; i++) {
+    int id = piece_id[i];
+    for (int j = 0; j < 3; ++j) {
+      int fi = F(j, i);
+      for (int k = 0; k < 3; ++k) {
+        float val = V(k, fi);
+        if (val < bbox_min(k, id)) bbox_min(k, id) = val;
+        if (val > bbox_max(k, id)) bbox_max(k, id) = val;
+      }
     }
   }
+  VectorXf bbox_width = (bbox_max - bbox_min).colwise().maxCoeff();
+  int max_id = 0;
+  float max_width = bbox_width[0];
+  for (int i = 1; i < num; ++i) {
+    if (bbox_width[i] > max_width) {
+      max_width = bbox_width[i];
+      max_id = i;
+    }
+  }
+  for (int i = 0; i < num; ++i) {
+    if (i != max_id) invalid_piece_id.push_back(i);
+  }  
+  // end of version 2
+
 
   VectorXb valid_f = VectorXb::Constant(nf, false);
   for (int i = 0; i < nf; i++) {
